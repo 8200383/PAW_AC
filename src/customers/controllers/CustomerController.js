56e3c4bc85862b678e1863f86bbb1f59c545e065
aimproxy <diasmicaelandre@gmail.com>
2022-04-13 17:08:52 +0100

    Add Customer Controller Prototype

---
+++ src/customers/controllers/CustomerController.js
@@ -0,0 +1,27 @@
+class CustomerController {
+    index(req, res) {
+
+    }
+
+    createCustomer(req, res) {
+
+    }
+
+    getAllCustomers(req, res) {
+
+    }
+
+    getCustomer(req, res) {
+
+    }
+
+    updateCustomer(req, res) {
+
+    }
+
+    deleteCustomer(req, res) {
+
+    }
+}
+
+module.exports = new CustomerController()
