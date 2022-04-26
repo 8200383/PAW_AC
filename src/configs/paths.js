44aa9d5c8818d29d2fd5c5911ed472365a1dab89
aimproxy <diasmicaelandre@gmail.com>
2022-04-26 20:54:27 +0100

    Fix views path

--- src/configs/paths.js
+++ src/configs/paths.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express')
 
 module.exports = framework => {
-    framework.set('views', path.join(__dirname, 'views'))
+    framework.set('views', path.join(__dirname, '../views'))
     framework.set('view engine', 'ejs')
     framework.use(express.static(path.join(__dirname, 'public')))
 }
