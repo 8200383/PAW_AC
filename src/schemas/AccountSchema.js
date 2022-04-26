5a74b51cb8d0e6061c5b342eb2779826f7d52c20
aimproxy <diasmicaelandre@gmail.com>
2022-04-26 15:34:41 +0100

    Add account schema

---
+++ src/schemas/AccountSchema.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const accountSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+const AccountModel = mongoose.model('Account', accountSchema);
+
+module.exports = AccountModel;
