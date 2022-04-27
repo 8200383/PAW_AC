06298c506be3bba42f115dcfe012c8b7184a8b6d
aimproxy <diasmicaelandre@gmail.com>
2022-04-27 15:05:26 +0100

    Update view styles

--- src/handlers/DashboardHandler.js
+++ src/handlers/DashboardHandler.js
@@ -1,5 +1,12 @@
 const index = async (req, res, next) => {
-    return res.render('index')
+    return res.render('index', {
+        navigation: [
+            { name: 'Customers', href: '/customers' },
+            { name: 'Employees', href: '/employees' },
+            { name: 'Purchases', href: '/purchases' },
+            { name: 'Books', href: '/books' },
+        ],
+    })
 }
 
 module.exports = {
