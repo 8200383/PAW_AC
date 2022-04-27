c4c27c207a5f1fac95d5497b56b95a526df6a176
aimproxy <diasmicaelandre@gmail.com>
2022-04-27 22:34:33 +0100

    Load Js files just one time

--- public/javascripts/pwa.js
+++ public/javascripts/pwa.js
@@ -1,24 +1,28 @@
 const loadModule = (module) => {
-    loadJavascript(`/javascripts/${module}.js`, document.body).then(() => {
-        console.log(`[!] ${module} has been loaded`)
-        onLoad()
-    })
+    if (document.getElementById(module) == null) {
+        loadJavascript(module, `/javascripts/${module}.js`, document.body).then(() => {
+            console.log(`[!] ${module} has been loaded`)
+            onLoad()
+        })
+    }
 }
 
 /**
  * Load Javascript file dynamiclly
  *
+ * @param {string} id
  * @param {string} url
  * @param {HTMLElement} location
  * @returns {Promise<*>}
  */
-var loadJavascript = (url, location) => {
+var loadJavascript = (id, url, location) => {
     //url is URL of external file, implementationCode is the code
     //to be called from the file, location is the location to
     //insert the <script> element
 
     return new Promise((resolve, reject) => {
         const scriptTag = document.createElement('script')
+        scriptTag.id = id
         scriptTag.type = 'text/javascript'
         scriptTag.src = url
 
