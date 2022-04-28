cf17be31acea81a40a1eaa0c34eecad09dc8eb32
Simao Santos <simaosantos01@hotmail.com>
2022-04-28 12:41:54 +0100

    bugfix

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,6 +1,6 @@
 module.exports = {
     AuthHandler: require('./AuthHandler'),
-    //DashboardHandler: require('./DashboardHandler'),
+    DashboardHandler: require('./DashboardHandler'),
     CustomersHandler: require('./CustomersHandler'),
     EmployeesHandler: require('./EmployeesHandler'),
     PurchasesHandler: require('./PurchasesHandler'),
