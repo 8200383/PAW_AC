3478868daa781b583c48531d526013b9d423ea2b
Simao <simaosantos01@hotmail.com>
2022-04-29 01:56:32 +0100

    remodelPurchaseTable func.

--- public/javascripts/purchases.js
+++ public/javascripts/purchases.js
@@ -1,4 +1,18 @@
 const onPurchases = () => {
     document.getElementById('title').innerText = 'Purchases'
     document.getElementById('action').innerText = 'Create purchase'
+
+    fetch('http://localhost:3000/api/purchases')
+        .then((res) => res.json())
+        .then((raw) => raw['purchases'])
+        .then((purchases) => {
+            if (purchases.length != 0) {
+                const columns = extractColumns(purchases[0])
+                renderTable(columns, purchases)
+                remodelPurchasesTable(purchases)
+            }
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 }
